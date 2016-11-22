0eb0a0bf317b79f6cd70879d779455a912337368
LukeDaywalker <liuxiaowei1215@126.com>
2016-11-23 00:26:31 +0800

    修改辅助命令

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
 @echo off
 adb shell pm uninstall -k com.wumii.android.mimi
 adb install gen\apk\mimi-5.4.2.signed.apk
+adb shell am start -n com.wumii.android.mimi/com.wumii.android.mimi.ui.activities.NavigationActivity
